1c736628f66828970d2c0a9faf20b96cd90fce99
NightByte <146828935+Saikarpe@users.noreply.github.com>
2025-11-27 14:22:09 +0530

    Update Q3381.java

--- Q3381.java
+++ Q3381.java
@@ -18,3 +18,32 @@ class Solution {
         return ans;
     }
 }
+//the above solving of the question was my approach and it solved the 653/661 test cases.
+import java.util.Arrays;
+
+class Solution {
+    public long maxSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        long[] prefix = new long[n + 1];
+        for (int i = 1; i <= n; i++) prefix[i] = prefix[i - 1] + nums[i - 1];
+
+        // min prefix sum seen for each remainder class (index % k)
+        long[] minPref = new long[k];
+        Arrays.fill(minPref, Long.MAX_VALUE / 4);
+        minPref[0] = 0L; // prefix[0] has remainder 0
+
+        long ans = Long.MIN_VALUE;
+        for (int r = 1; r <= n; r++) {
+            int mod = r % k;
+            // possible candidate: prefix[r] - smallest earlier prefix with same mod
+            if (minPref[mod] != Long.MAX_VALUE / 4) {
+                ans = Math.max(ans, prefix[r] - minPref[mod]);
+            }
+            // update min prefix for this remainder with current prefix[r]
+            if (prefix[r] < minPref[mod]) minPref[mod] = prefix[r];
+        }
+
+        return ans;
+    }
+}
+//this is the solution which is provided by chatgpt
